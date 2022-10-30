4bed8fa813cd58c500147303fd40f202a99e4e45
iuliia-sokol <110597796+iuliia-sokol@users.noreply.github.com>
2022-10-30 18:18:15 +0000

    Deploying to gh-pages from @ iuliia-sokol/goit-js-hw-09@6af5088db235c0368ff46aab15f9f720edf22888 🚀

--- 01-color-switcher.92b40657.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");let o=null;t.addEventListener("click",(function(t){o=setInterval((function(){n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.target.disabled=!0}),1e3)})),e.addEventListener("click",(function(e){clearInterval(o),t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.92b40657.js.map
