fef592b84285d3db8e1e4b33c1ca18eaaf092bfb
Iuliia Sokol <avare779@gmail.com>
2022-10-30 20:01:06 +0200

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,41 +6,58 @@
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const submitBtnEl = document.querySelector('button');
+const formEl = document.querySelector('.form');
 const delayInptEl = document.querySelector('[ name="delay"]');
 const stepInptEl = document.querySelector('[ name="step"]');
 const amountInptEl = document.querySelector('[ name="amount"]');
-// console.log(delayInptEl);
 
-submitBtnEl.addEventListener('click', createPromise(delayInptEl.value));
+const notifyOptions = {
+  width: '380px',
+  position: 'right-top',
+  distance: '10px',
+  opacity: 1,
+  fontSize: '20px',
+  borderRadius: '12px',
+};
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    Notify.success('Please choose a date in the future', {
-      width: '380px',
-      position: 'right-top',
-      distance: '10px',
-      opacity: 1,
-      fontSize: '20px',
-      borderRadius: '12px',
-    });
-  } else {
-    Notify.failure('Please choose a date in the future', {
-      width: '380px',
-      position: 'right-top',
-      distance: '10px',
-      opacity: 1,
-      fontSize: '20px',
-      borderRadius: '12px',
-    });
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  let delay = +delayInptEl.value;
+  let step = +stepInptEl.value;
+  let amount = +amountInptEl.value;
+
+  // console.log(delay, step, amount);
+
+  for (let i = 0; i < amount; i++) {
+    let delayedPromise = delay + step * i;
+    createPromise(i + 1, delayedPromise)
+      .then(({ position, delay }) =>
+        Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`,
+          notifyOptions
+        )
+      )
+      .catch(({ position, delay }) =>
+        Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`,
+          notifyOptions
+        )
+      );
   }
 }
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
